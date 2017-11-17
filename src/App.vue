bd0cec101318a5c30eadaf058eaf914e80e245ce
dingzhaohua <1207470844@qq.com>
2017-11-17 13:52:14 +0800

    去除多余文件【start coding】
    去除多余文件【start coding】

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,15 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
     <router-view/>
-    <el-button type="primary" @click="testApi">TEST</el-button>
   </div>
 </template>
 
 <script>
-import Test from 'api/test';
 export default {
-  name: "app",
-  methods: {
-    testApi() {
-      Test.test({
-        id: 1
-      }).then(res => {
-        console.log('resTestApi', res);
-      })
-    }
-  }
+  name: "app"
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '~normalize.css/normalize.css'; //normalize.css 样式格式化
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Test from '@/components/Test'
 import Login from 'views/login/login';
 
 Vue.use(Router)
@@ -10,16 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
-    },
-    {
-      path: '/test',
-      name: 'Test',
-      component: Test
-    },
-    {
-      path: '/login',
       name: 'Login',
       component: Login
     }
