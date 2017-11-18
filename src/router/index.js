d784c2fa3a911f1dc4d6b1f291d9f5685ff8b65a
丁昭华 <1207470844@qq.com>
2017-11-18 23:46:53 +0800

    router使用hash模式【暂时】

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const _import = require('./_import_' + process.env.NODE_ENV);
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   linkActiveClass: 'router-link-active',
   routes: [{
     path: '/login',
