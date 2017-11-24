8db0ddac068d3b0cb4938ebff30e367c6d43f7b5
dingzhaohua <1207470844@qq.com>
2017-11-24 16:29:56 +0800

    使用webpack require.context去中心化处理
    使用webpack require.context去中心化处理

--- src/components/icons-svg/index.vue
+++ src/components/icons-svg/index.vue
@@ -9,10 +9,12 @@
  * @Author: dzh 
  * @Date: 2017-11-24 16:04:16 
  * @Last Modified by: dzh
- * @Last Modified time: 2017-11-24 16:04:40
+ * @Last Modified time: 2017-11-24 16:29:19
  */
 
-//http://blog.csdn.net/zb0567/article/details/77987727?locationNum=9&fps=1
+// 使用 webpack 的 require.context 实现路由“去中心化”管理 https://www.v2ex.com/t/359380
+
+// webpack 插件 svg-sprite-loader的使用 http://blog.csdn.net/zb0567/article/details/77987727?locationNum=9&fps=1
 
 // requires and returns all modules that match
 const requireAll = requireContext => requireContext.keys().map(requireContext);
