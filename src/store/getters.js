3498c76e18e16e724a3ead71b9f251274f0deb75
dingzhaohua <1207470844@qq.com>
2017-11-24 14:02:20 +0800

    添加vuex说明
    添加vuex说明

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,11 +1,14 @@
 /*
  * @Author: dzh 
  * @Date: 2017-11-24 13:38:20 
- * @Last Modified by:   dzh 
- * @Last Modified time: 2017-11-24 13:38:20 
+ * @Last Modified by: dzh
+ * @Last Modified time: 2017-11-24 14:00:49
+ * 
+ * 要在页面中使用的vuex数据，先在getters中统一返回，页面中使用mapGetters取得
+ * 
  */
 const getters = {
-
+  username: state => state.auth.username
 };
 
 export default getters;
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@
  * @Author: dzh 
  * @Date: 2017-11-24 09:53:26 
  * @Last Modified by: dzh
- * @Last Modified time: 2017-11-24 13:44:06
+ * @Last Modified time: 2017-11-24 13:58:10
  * 
  * vuex中的数据变更仅使用mutations(不用actions)，而且仅仅负责变更数据，其他逻辑全都提取到各个功能模块中
- * 
+ * 变更数据只使用vuex的mutations，且在mutations中只负责变更数据，逻辑处理全都抽离
  */
 import Vue from 'vue';
 import Vuex from 'vuex';
