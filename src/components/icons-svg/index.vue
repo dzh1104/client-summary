217221abfa06884f2bad4700f5484461e3bd341a
dzh1104 <1207470844@qq.com>
2017-12-09 22:26:21 +0800

    添加icon view

--- src/components/icons-svg/index.vue
+++ src/components/icons-svg/index.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import generateIconsView from '@/views/svg-icons/generateIconsView.js';
+
 // 使用 webpack 的 require.context 实现路由“去中心化”管理 https://www.v2ex.com/t/359380
 
 // webpack 插件 svg-sprite-loader的使用 http://blog.csdn.net/zb0567/article/details/77987727?locationNum=9&fps=1
@@ -13,8 +15,9 @@
 const requireAll = requireContext => requireContext.keys().map(requireContext);
 // import all svg
 const req = require.context('./svg', false, /\.svg$/);
-requireAll(req);
+const iconMap = requireAll(req);
 
+generateIconsView.generate(iconMap);
 // 使用:
 //  1.先去阿里的iconfont下载图标 文件名为ABC.svg文件到icons-svg/svg下
 //  2.在页面中使用全局注册的组件<icon-svg icon-class="ABC"></icon-svg>
--- src/views/svg-icons/index.vue
+++ src/views/svg-icons/index.vue
@@ -1,10 +1,67 @@
 <style lang="scss" scoped>
+.m-icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10px 20px 0;
+  .icon-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 120px;
+    height: 120px;
+    margin: 8px;
+    font-size: 18px;
+    text-align: center;
+    // border: 1px dotted blue;
+    // border-radius: 10px;
+    .u-icon {
 
+    }
+  }
+}
 </style>
 
-
 <template>
-  <div>
-    svg-icons
+  <div class="m-icons">
+    <div class="icon-item f-cur" v-for="(item, index) in iconsMap" :key="index" @click="handleClipboard(generateIconCode(item),$event)">
+      <el-tooltip placement="top">
+        <div slot="content">
+          {{generateIconCode(item)}}
+        </div>
+        <div class="u-icon">
+          <svg-icon :icon-class="item"></svg-icon>
+          <p>{{item}}</p>
+        </div>
+      </el-tooltip>
+    </div>
   </div>
 </template>
+
+<script>
+import icons from "./generateIconsView";
+import clipboard from "utils/clipboard";
+
+export default {
+  name: "icons",
+  data() {
+    return {
+      iconsMap: []
+    };
+  },
+  mounted() {
+    const iconsMap = icons.state.iconsMap.map(i => {
+      return i.default.id.split("-")[1];
+    });
+    this.iconsMap = iconsMap;
+  },
+  methods: {
+    generateIconCode(symbol) {
+      return `<svg-icon icon-class="${symbol}" />`;
+    },
+    handleClipboard(text, event) {
+      clipboard(text, event);
+    }
+  }
+};
+</script>
+
