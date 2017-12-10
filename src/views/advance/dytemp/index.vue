773a9fa8338603e4aa49567932a7d007289d8870
dzh1104 <1207470844@qq.com>
2017-12-10 22:20:26 +0800

    动态模板

--- src/views/advance/dytemp/index.vue
+++ src/views/advance/dytemp/index.vue
@@ -6,14 +6,13 @@
 
 <template>
   <div class="m-dytemp">
-    <h3>动态模板</h3>
-    <el-button type="primary" @click="generateTemp">生成模板</el-button>
+    <h3>动态模板$options.template</h3>
     <code v-html="content1"></code>
     <code>
       <dytempItem1 :content="content2"></dytempItem1>
     </code>
     <code>
-      <dytempItem2 :content="content2"></dytempItem2>
+      <dytempItem2 :content="content2" @triggerBySubClick="triggerBySubClick"></dytempItem2>
     </code>
   </div>
 </template>
@@ -23,16 +22,23 @@ export default {
   data() {
     return {
       // 使用原生html写法onclick可以，使用vue语法绑定点击事件不生效
-      content1: `<p @click="alert('v-html vue绑定事件')">这是段落1</p><p onclick="alert('v-html html绑定事件')">这是段落2</p>`,
-      content2: `<p @click="handleClick('v-html vue绑定事件')">这是段落1</p><p onclick="alert('v-html html绑定事件')">这是段落2</p>`
+      content1: `<p @click="handleClick('v-html vue绑定事件')" class="f-cur">这是段落1</p><p onclick="alert('v-html html绑定事件')" class="f-cur">这是段落2</p>`,
+      content2: `<p @click="handleClick('v-html vue绑定事件')" class="f-cur">这是段落1</p><p onclick="alert('v-html html绑定事件')" class="f-cur">这是段落2</p>`
     }
   },
   methods: {
-    generateTemp() {
-
+    triggerBySubClick() {
+      this.$message({
+        message: '子组件动态模板上绑定的click事件触发自定义事件',
+        type: 'success'
+      })
     }
   },
   components: {
+    /**
+     * 组件渲染过程 : template先于props
+     * 所以下面的组件dytempItem1显示undefined，使用动态模板this.$opeions.template设置可以渲染props穿过来的模板
+     */
     dytempItem1: {
       props: {
         content: String
@@ -58,6 +64,7 @@ export default {
       methods: {
         handleClick(str) {
           alert(str);
+          this.$emit('triggerBySubClick');
         }
       }
     }
