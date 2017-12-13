7d980cd21dbcb7f96fd02d8c4c6e93ce4e7141f4
dzh1104 <1207470844@qq.com>
2017-12-13 11:46:49 +0800

    升级element ui2.0.8 fix tooltip
    升级element ui2.0.8 fix tooltip

--- src/views/components/backToTop/index.vue
+++ src/views/components/backToTop/index.vue
@@ -63,7 +63,7 @@
     <code>我是占位</code>
     <!--可自定义按钮的样式、show/hide临界点、返回的位置  -->
     <!--如需文字提示，可在外部添加element的<el-tooltip></el-tooltip>元素  -->
-    <el-tooltip placement="top" content="文字提示">
+    <el-tooltip placement="top" content="回到顶部" trigger="hover">
       <back-to-top transitionName="fade" :visibilityHeight="200"></back-to-top>
     </el-tooltip>
   </div>
