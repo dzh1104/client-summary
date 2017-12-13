543c1e587ba8f807df475a22edf1682785beb7c8
dzh1104 <1207470844@qq.com>
2017-12-13 12:55:20 +0800

    add: 登录页面canvas动画
    add: 登录页面canvas动画

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -32,6 +32,15 @@
       }
     }
   }
+  > canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+  }
 }
 </style>
 
@@ -84,6 +93,8 @@
       </el-button>
     </el-form-item>
   </el-form>
+
+  <canvas width="100%" height="100%"></canvas>
 </div>
 </template>
 
@@ -111,7 +122,7 @@ export default {
         let password = this.loginInfo.password;
         AuthApi.login(username, password).then(res => {
           if (typeof res !== "number") {
-            this.$router.push({ path: '/' })
+            this.$router.push({ path: "/" });
           }
         });
       } else {
@@ -128,6 +139,59 @@ export default {
     changeOperate() {
       this.type = !this.type;
     }
+  },
+  mounted() {
+    var login = document.getElementsByClassName('m-login')[0];
+    login.addEventListener("touchmove", function(e) {
+      e.preventDefault();
+    });
+    var c = document.getElementsByTagName("canvas")[0],
+      x = c.getContext("2d"),
+      pr = window.devicePixelRatio || 1,
+      w = window.innerWidth,
+      h = window.innerHeight,
+      f = 90,
+      q,
+      m = Math,
+      r = 0,
+      u = m.PI * 2,
+      v = m.cos,
+      z = m.random;
+    c.width = w * pr;
+    c.height = h * pr;
+    x.scale(pr, pr);
+    x.globalAlpha = 0.6;
+    function i() {
+      x.clearRect(0, 0, w, h);
+      q = [{ x: 0, y: h * 0.7 + f }, { x: 0, y: h * 0.7 - f }];
+      while (q[1].x < w + f) d(q[0], q[1]);
+    }
+    function d(i, j) {
+      x.beginPath();
+      x.moveTo(i.x, i.y);
+      x.lineTo(j.x, j.y);
+      var k = j.x + (z() * 2 - 0.25) * f,
+        n = y(j.y);
+      x.lineTo(k, n);
+      x.closePath();
+      r -= u / -50;
+      x.fillStyle =
+        "#" +
+        (((v(r) * 127 + 128) << 16) |
+          ((v(r + u / 3) * 127 + 128) << 8) |
+          (v(r + u / 3 * 2) * 127 + 128)
+        ).toString(16);
+      x.fill();
+      q[0] = q[1];
+      q[1] = { x: k, y: n };
+    }
+    function y(p) {
+      var t = p + (z() * 2 - 1.1) * f;
+      return t > h || t < 0 ? y(p) : t;
+    }
+    document.onclick = i;
+    document.ontouchstart = i;
+    i();
   }
 };
 </script>
