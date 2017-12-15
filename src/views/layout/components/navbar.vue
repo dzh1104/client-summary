86ed1ef3be1845fed8ab8e30580c7e0797926f27
dzh1104 <1207470844@qq.com>
2017-12-15 14:59:33 +0800

    add: fullscreen
    add: fullscreen

--- src/views/layout/components/navbar.vue
+++ src/views/layout/components/navbar.vue
@@ -1,17 +1,27 @@
 <style lang="scss" scoped>
 .m-navbar {
+  height: 50px;
+  line-height: 50px;
+  border-radius: 0px !important;
   .m-hamburger {
     float: left;
     height: 50px;
+    line-height: 58px;
     padding: 0 10px;
-    line-height: 50px;
   }
   .m-operation-area {
     float: right; // 父级已设置高度 不需要用伪类清除浮动
-    height: 100%;
-    margin-right: 10px;
+    height: 50px;
+    .m-screenfull {
+      float: left;
+      height: 20px;
+      margin-top: 15px;
+      margin-right: 8px;
+    }
     .m-avatar-container {
-      height: 100%;
+      float: left;
+      height: 50px;
+      margin-right: 8px;
       // margin-top: 5px;
       .avatar-wrap {
         .u-img {
@@ -33,27 +43,31 @@
 
     <!-- 右侧操作区 -->
     <div class="m-operation-area">
-        <el-dropdown trigger="click" class="m-avatar-container">
-            <div class="avatar-wrap f-cur">
-                <img class="u-img" :src="avatar" alt="avatar">
-                <i class="el-icon-caret-bottom"></i>
-            </div>
-            <el-dropdown-menu slot="dropdown">
-                <router-link to="/">
-                    <el-dropdown-item>
-                        首页
-                    </el-dropdown-item>
-                </router-link>
-                <a target="_blank" href="https://github.com/dzh1104/client-summary">
-                    <el-dropdown-item>
-                        项目地址
-                    </el-dropdown-item>
-                </a>
-                <el-dropdown-item divided>
-                    <span @click="logout">退出登录</span>
-                </el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>
+      <el-tooltip effect="dark" content="全屏" placement="bottom">
+        <screenfull class="m-screenfull"></screenfull>
+      </el-tooltip>
+
+      <el-dropdown trigger="click" class="m-avatar-container">
+          <div class="avatar-wrap f-cur">
+              <img class="u-img" :src="avatar" alt="avatar">
+              <i class="el-icon-caret-bottom"></i>
+          </div>
+          <el-dropdown-menu slot="dropdown">
+              <router-link to="/">
+                  <el-dropdown-item>
+                      首页
+                  </el-dropdown-item>
+              </router-link>
+              <a target="_blank" href="https://github.com/dzh1104/client-summary">
+                  <el-dropdown-item>
+                      项目地址
+                  </el-dropdown-item>
+              </a>
+              <el-dropdown-item divided>
+                  <span @click="logout">退出登录</span>
+              </el-dropdown-item>
+          </el-dropdown-menu>
+      </el-dropdown>
     </div>
   </el-menu>
 </template>
@@ -61,7 +75,8 @@
 <script>
 import { mapGetters } from "vuex";
 import hamburger from "components/hamburger";
-import authApi from 'api/auth';
+import screenfull from "components/screenfull";
+import authApi from "api/auth";
 export default {
   data() {
     return {
@@ -75,12 +90,13 @@ export default {
   methods: {
     logout() {
       authApi.clientLogout().then(res => {
-        this.$router.push({ path: '/login' });
-      })
+        this.$router.push({ path: "/login" });
+      });
     }
   },
   components: {
-    hamburger
+    hamburger,
+    screenfull
   }
 };
 </script>
